64c6d618555cded05e63d83b95b509f990562bf4
sachinrg17 <46643078+sachinrg17@users.noreply.github.com>
2020-04-07 09:42:05 +0530

    Add files via upload

---
+++ camp/js/mini.js
@@ -0,0 +1,10 @@
+$('.slick-slider').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    dots: true,
+    arrows: true,
+    swipe: true,
+  //  infinite: true,
+    swipeToSlide: true,
+    //adaptiveHeight: true,
+  });
